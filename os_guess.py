1028d9bd516f502fa5aecccc01b42d64f8226cd0
4n0mal1a <129337574+4n0mal1a@users.noreply.github.com>
2023-05-07 20:30:25 +0200

    Update os_guess.py for work no linux

--- os_guess.py
+++ os_guess.py
@@ -6,102 +6,121 @@ import re, sys, subprocess
     print("\n[!] USO: python " + sys.argv[0] + " <ip_address>\n")
     sys.exit(1)
 """
+if os.name == "posix":
+    def get_ttl(ip_address):
+        try:
+            proc = subprocess.Popen(["ping", "-n", "1", ip_address], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out, err = proc.communicate()
 
-def get_ttl(ip_address):
-    try:
-        proc = subprocess.Popen(["ping", "-n", "1", ip_address], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = proc.communicate()
+            out = out.split()
+            out = out[14].decode()
+
+            ttl = re.findall(r"\d{1,3}", out)[0]
+
+            return ttl
+        except IndexError:
+            return 1000
+
+
+    def get_os(ttl):
+
+        ttl = int(ttl)
+
+        if ttl >= 0 and ttl <= 64:
+            return "Linux"
+        elif ttl >= 65 and ttl <= 128:
+            return "Windows"
+        else:
+            return "Not Found"
+
+
+    def init_guess(ip):
+        try:
+            ttl = get_ttl(ip)
+
+            os_name = get_os(ttl)
+
+            if ttl == 1000:
+                ttl = "N/A"
+            return ("(ttl --> {}): {}".format(ttl, os_name))
+
+        except TypeError:
+            print("\n[!] USO: python " + sys.argv[0] + " <ip_address>\n")
+            sys.exit(1)
+
+
+    if __name__ == "__main__":
+        try:
+            ip_address = sys.argv[1]
+
+            ttl = get_ttl(ip_address)
+
+            os_name = get_os(ttl)
+
+            print("{} (ttl --> {}): {}".format(ip_address, ttl, os_name))
+
+        except TypeError:
+            print("\n[!] USO: python " + sys.argv[0] + " <ip_address>\n")
+            sys.exit(1)
+else:
+    # !/usr/bin/python
+
+    import re, sys, subprocess
+
+    # python wichSystem.py 127.0.0.1
+
+    if len(sys.argv) != 2:
+        print("\n[!] USO: python " + sys.argv[0] + " <ip_address>\n")
+        sys.exit(1)
+
+
+    def get_ttl(ip_addres):
+
+        proc = subprocess.Popen(["/usr/bin/ping -c 1 %s" % ip_address, ""], stdout=subprocess.PIPE, shell=True)
+        (out, err) = proc.communicate()
 
         out = out.split()
-        out = out[14].decode()
+        out = out[12].decode()
 
         ttl = re.findall(r"\d{1,3}", out)[0]
-
         return ttl
-    except IndexError:
-        return 1000
 
 
-def get_os(ttl):
+    def get_os(ttl):
 
-    ttl = int(ttl)
+        ttl = int(ttl)
 
-    if ttl >= 0 and ttl <= 64:
-        return "Linux"
-    elif ttl >= 65 and ttl <= 128:
-        return "Windows"
-    else:
-        return "Not Found"
+        if ttl >= 0 and ttl <= 64:
+            return "Linux"
+        elif ttl >= 65 and ttl <= 128:
+            return "Windows"
+        else:
+            return "Not Found"
 
+    def init_guess(ip):
+        try:
+            ttl = get_ttl(ip)
 
-def init_guess(ip):
-    try:
-        ttl = get_ttl(ip)
+            os_name = get_os(ttl)
 
-        os_name = get_os(ttl)
+            if ttl == 1000:
+                ttl = "N/A"
+            return ("(ttl --> {}): {}".format(ttl, os_name))
 
-        if ttl == 1000:
-            ttl = "N/A"
-        return ("(ttl --> {}): {}".format(ttl, os_name))
+        except TypeError:
+            print("\n[!] USO: python " + sys.argv[0] + " <ip_address>\n")
+            sys.exit(1)
 
-    except TypeError:
-        print("\n[!] USO: python " + sys.argv[0] + " <ip_address>\n")
-        sys.exit(1)
+    if __name__ == "__main__":
+        try:
+            ip_address = sys.argv[1]
 
+            ttl = get_ttl(ip_address)
 
-if __name__ == "__main__":
-    try:
-        ip_address = sys.argv[1]
+            os_name = get_os(ttl)
 
-        ttl = get_ttl(ip_address)
+            print("{} (ttl --> {}): {}".format(ip_address, ttl, os_name))
 
-        os_name = get_os(ttl)
-
-        print("{} (ttl --> {}): {}".format(ip_address, ttl, os_name))
-
-    except TypeError:
-        print("\n[!] USO: python " + sys.argv[0] + " <ip_address>\n")
-        sys.exit(1)
-
-"""import re
-import sys
-import subprocess
-
-if len(sys.argv) != 2:
-    print("\n[!] USO: python " + sys.argv[0] + " <ip_address>\n")
-    sys.exit(1)
-
-def get_ttl(ip_address):
-    proc = subprocess.Popen(["ping", "-n", "1", ip_address], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = proc.communicate()
-
-    out = out.decode()
-    ttl = re.findall(r"TTL=\d+", out)
-    if ttl:
-        return int(ttl[0].split('=')[1])
-    else:
-        return None
-
-def get_os(ttl):
-    if ttl is None:
-        return "Not Found"
-    elif ttl >= 0 and ttl <= 64:
-        return "Linux/Unix"
-    elif ttl >= 65 and ttl <= 128:
-        return "Windows"
-    else:
-        return "Not Found"
-
-if __name__ == "__main__":
-    try:
-        ip_address = sys.argv[1]
-
-        ttl = get_ttl(ip_address)
-
-        os_name = get_os(ttl)
-
-        print("{} (ttl --> {}): {}".format(ip_address, ttl, os_name))
-
-    except TypeError:
-        print("\n[!] USO: python " + sys.argv[0] + " <ip_address>\n")
-        sys.exit(1)"""
+        except TypeError:
+            print("\n[!] USO: python " + sys.argv[0] + " <ip_address>\n")
+            sys.exit(1)
